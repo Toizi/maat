34407fd1848400c0393e9cddd9da84130dc94f02
Marius Guggenmos <3457016+Toizi@users.noreply.github.com>
2025-01-19 22:18:30 +0100

    more docs

--- bindings/python/py_maat.cpp
+++ bindings/python/py_maat.cpp
@@ -8,18 +8,79 @@ namespace maat
 namespace py
 {
 
+PyDoc_STRVAR(
+    maat_Cst_doc,
+    "Cst(size: int, value: int|str, base: Optional[int]=16) -> Value\n"
+    "\n"
+    "Create a constant abstract expression.\n"
+    "\n"
+    ":param int size: Size of the value in bits. Must be greater than 64 if `value` is a `str`\n"
+    ":param int|str value: Constant value of the expression\n"
+    ":param Optional[int] base: Base of `value` if `value` is a `str`"
+);
+PyDoc_STRVAR(
+    maat_Var_doc,
+    "Var(size: int, name: str) -> Value\n"
+    "\n"
+    "Create an abstract variable.\n"
+    "\n"
+    ":param int size: Size of the expression in bits\n"
+    ":param str name: Unique name identifying the variable"
+);
+PyDoc_STRVAR(
+    maat_Concat_doc,
+    "Concat(upper: Value, lower: Value) -> Value\n"
+    "\n"
+    "Concatenate two abstract expressions"
+);
+PyDoc_STRVAR(
+    maat_Extract_doc,
+    "Extract(val: Value, higher: int, lower: int) -> Value\n"
+    "\n"
+    "Bitfield extract from an abstract expression"
+);
+PyDoc_STRVAR(
+    maat_Sext_doc,
+    "Sext(new_size: int, val: Value) -> Value\n"
+    "\n"
+    "Sign-extend an abstract value"
+);
+PyDoc_STRVAR(
+    maat_Zext_doc,
+    "Zext(new_size: int, val: Value) -> Value\n"
+    "\n"
+    "Zero-extend an abstract value"
+);
+PyDoc_STRVAR(
+    maat_ULE_doc,
+    "ULE(left: int|Value, right: int|Value) -> Constraint\n"
+    "\n"
+    "Create an unsigned less-equal constraint. At least one of left or right must be a `Value`."
+);
+PyDoc_STRVAR(
+    maat_ULT_doc,
+    "ULT(left: int|Value, right: int|Value) -> Constraint\n"
+    "\n"
+    "Create an unsigned less-than constraint. At least one of left or right must be a `Value`."
+);
+PyDoc_STRVAR(
+    maat_ITE_doc,
+    "ITE(constraint: Constraint, if_true: Value|int, if_false: Value|int) -> Constraint\n"
+    "\n"
+    "Create an If-Then-Else expression from a Constraint and two abstract expressions"
+);
 // Module methods
 PyMethodDef module_methods[] = {
     // Expressions
-    {"Cst", (PyCFunction)maat_Cst, METH_VARARGS | METH_KEYWORDS, "Create a constant abstract expression"},
-    {"Var", (PyCFunction)maat_Var, METH_VARARGS | METH_KEYWORDS, "Create an abstract variable expression"},
-    {"Concat", (PyCFunction)maat_Concat, METH_VARARGS, "Concatenate two abstract expressions"},
-    {"Extract", (PyCFunction)maat_Extract, METH_VARARGS, "Bitfield extract from an abstract expression"},
-    {"Sext", (PyCFunction)maat_Sext, METH_VARARGS, "Sign-extend an abstract value"},
-    {"ULE", (PyCFunction)maat_ULE, METH_VARARGS, "Unsigned less-equal constraint on abstract values"},
-    {"ULT", (PyCFunction)maat_ULT, METH_VARARGS, "Unsigned less-than constraint on abstract values"},
-    {"Zext", (PyCFunction)maat_Zext, METH_VARARGS, "Zero-extend an abstract value"},
-    {"ITE", (PyCFunction)maat_ITE, METH_VARARGS, "Create an If-Then-Else expression from a Constraint and two abstract expressions"},
+    {"Cst", (PyCFunction)maat_Cst, METH_VARARGS | METH_KEYWORDS, maat_Cst_doc},
+    {"Var", (PyCFunction)maat_Var, METH_VARARGS | METH_KEYWORDS, maat_Var_doc},
+    {"Concat", (PyCFunction)maat_Concat, METH_VARARGS, maat_Concat_doc},
+    {"Extract", (PyCFunction)maat_Extract, METH_VARARGS, maat_Extract_doc},
+    {"Sext", (PyCFunction)maat_Sext, METH_VARARGS, maat_Sext_doc},
+    {"Zext", (PyCFunction)maat_Zext, METH_VARARGS, maat_Zext_doc},
+    {"ULE", (PyCFunction)maat_ULE, METH_VARARGS, maat_ULE_doc},
+    {"ULT", (PyCFunction)maat_ULT, METH_VARARGS, maat_ULT_doc},
+    {"ITE", (PyCFunction)maat_ITE, METH_VARARGS, maat_ITE_doc},
     // EVM
     {"EVMTransaction", (PyCFunction)maat_Transaction, METH_VARARGS, "Create an ethereum transaction"},
     {"contract", (PyCFunction)maat_contract, METH_VARARGS, "Get EVM contract associated with a MaatEngine"},
